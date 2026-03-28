773d0ce08690511c0127d04cc1087b8a91b0f677
Yasin Sabanoglu <sabanoglu2006@gmail.com>
2026-03-28 01:11:01 +0100

    Add monster class

--- terminal_legends/monster.py
+++ terminal_legends/monster.py
@@ -1,1 +1,17 @@
+class Monster:
+    def __init__(self, name, hp, attack, xp_reward):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.xp_reward = xp_reward
 
+    def is_alive(self):
+        return self.hp > 0
+
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
+
+    def deal_damage(self):
+        return self.attack
