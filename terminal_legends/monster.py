9339ef777d5b6d26e92ef05c1571196e9ff72db1
Yasin Sabanoglu <sabanoglu2006@gmail.com>
2026-03-28 13:44:25 +0100

    Add gold rewards to monsters

--- terminal_legends/monster.py
+++ terminal_legends/monster.py
@@ -1,9 +1,10 @@
 class Monster:
-    def __init__(self, name, hp, attack, xp_reward):
+    def __init__(self, name, hp, attack, xp_reward, gold_reward):
         self.name = name
         self.hp = hp
         self.attack = attack
         self.xp_reward = xp_reward
+        self.gold_reward = gold_reward
 
     def is_alive(self):
         return self.hp > 0
@@ -15,4 +16,3 @@ class Monster:
 
     def deal_damage(self):
         return self.attack
-        
