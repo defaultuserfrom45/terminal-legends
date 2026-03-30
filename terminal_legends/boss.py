8370461548fdede605df61e1bf20029fc2d589ea
Yasin Sabanoglu <sabanoglu2006@gmail.com>
2026-03-30 14:25:27 +0200

    Update boss.py

--- terminal_legends/boss.py
+++ terminal_legends/boss.py
@@ -6,13 +6,13 @@ class Boss(Monster):
         super().__init__(name, hp, attack, xp_reward, gold_reward)
 
     def use_special_attack(self):
-        return self.attack + 5
+        return self.attack + 4
 
 
 def create_boss(player_level):
-    boss_hp = 35 + player_level * 5
-    boss_attack = 8 + player_level
-    boss_xp = 30
-    boss_gold = 25
+    boss_hp = 32 + player_level * 5
+    boss_attack = 7 + player_level
+    boss_xp = 35
+    boss_gold = 30
 
     return Boss("Dragon", boss_hp, boss_attack, boss_xp, boss_gold)
