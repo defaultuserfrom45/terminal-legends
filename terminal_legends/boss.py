3d48c2c43e859d45978fafc12af0fc464f6ccb89
Yasin Sabanoglu <sabanoglu2006@gmail.com>
2026-03-30 13:46:50 +0200

    Upgrade boss to special boss class

--- terminal_legends/boss.py
+++ terminal_legends/boss.py
@@ -1,10 +1,18 @@
 from terminal_legends.monster import Monster
 
 
+class Boss(Monster):
+    def __init__(self, name, hp, attack, xp_reward, gold_reward):
+        super().__init__(name, hp, attack, xp_reward, gold_reward)
+
+    def use_special_attack(self):
+        return self.attack + 5
+
+
 def create_boss(player_level):
     boss_hp = 35 + player_level * 5
     boss_attack = 8 + player_level
     boss_xp = 30
     boss_gold = 25
 
-    return Monster("Dragon", boss_hp, boss_attack, boss_xp, boss_gold)
+    return Boss("Dragon", boss_hp, boss_attack, boss_xp, boss_gold)
